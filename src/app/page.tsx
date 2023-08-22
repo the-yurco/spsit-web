596e603f403861b167c5f3a5b916234537310646
jurco-exe <jurajbosy1@gmail.com>
2023-08-22 17:05:31 +0200

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from 'next/image'
 
+import {RxDoubleArrowDown} from 'react-icons/rx'
+
 export default function Home() {
   return (
-    <section>
-      <h1>home page</h1>
+    <section id='hero-section' className='h-screen w-screen relative top-0 grid grid-rows-6'>
+      <div className='flex justify-center row-start-5 row-end-6'>
+        <ul className='flex gap-8 text-xl font-semibold'>
+          <li><a href="">Aktuality</a></li>
+          <li><a href="">O nás</a></li>
+          <li><a href="">Študijné odbory</a></li>
+          <li><a href="">Edupage</a></li>
+          <li><a href="">Novinky</a></li>
+          <li><a href="">Kontakt</a></li>
+        </ul>
+      </div>
+      <div className='flex justify-center row-start-6 row-end-7'>
+        <div className='flex gap-8 text-4xl font-semibold'>
+          <RxDoubleArrowDown/>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,32 @@ import Image from 'next/image'
 
 import logo from '/public/logo.svg';
 
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {FaUser}from 'react-icons/fa6'
+
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <>
+    <section className="w-screen mt-16 absolute">
+        <div className="container mx-auto relative flex justify-between z-10 px-16">
         <div>
-            <Image src={logo} alt={''} width={300} height={300}/>
+                <Image src={logo} alt={''} width={230} height={150}/>
+            </div>
+            <div className='flex justify-between gap-10'>
+                <div className='flex justify-between items-center gap-1'>
+                    <div className=' text-mainGreen text-4xl'>
+                        <GiHamburgerMenu/>
+                    </div>
+                    <h3 className=' text-mainGreen uppercase font-semibold'>Menu</h3>
+                </div>
+                <div className='flex justify-between items-center text-mainGreen text-2xl'>
+                    <FaUser/>
+                </div>
+            </div>
         </div>
-    </>
+    </section>
+    
   )
 }
 
